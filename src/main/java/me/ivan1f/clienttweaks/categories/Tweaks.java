edf2ce032cf58cee5beb572e5a92eab8363988ca
Ivan1F <wyf0762@foxmail.com>
2021-10-30 18:09:12 +0800

    immediatelyRespawn

--- src/main/java/me/ivan1f/clienttweaks/categories/Tweaks.java
+++ src/main/java/me/ivan1f/clienttweaks/categories/Tweaks.java
@@ -1,10 +1,15 @@
 package me.ivan1f.clienttweaks.categories;
 
 import me.ivan1f.quickconfig.setting.Category;
+import me.ivan1f.quickconfig.setting.ChangeHandler;
 import me.ivan1f.quickconfig.setting.Setting;
 import me.ivan1f.quickconfig.setting.WithHotkey;
+import net.minecraft.client.MinecraftClient;
+
+import java.util.function.Consumer;
 
 @Category
+@SuppressWarnings("unused")
 public class Tweaks {
     @Setting
     @WithHotkey
@@ -18,4 +23,13 @@ public class Tweaks {
     @Setting
     @WithHotkey
     public static boolean renderItemRemoveCountDown = false;
+    @Setting
+    @WithHotkey
+    public static boolean immediatelyRespawn = false;
+    @ChangeHandler(of = "immediatelyRespawn")
+    public static Consumer<Boolean> immediatelyRespawnChangeHandler = (value) -> {
+        if (MinecraftClient.getInstance().player != null) {
+            MinecraftClient.getInstance().player.setShowsDeathScreen(!value);
+        }
+    };
 }
